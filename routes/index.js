918084ffd5dbbf32faf53b55b063b95b3aa3b8ce
Camilla Freitas <camirsfreitas@gmail.com>
2020-10-14 18:59:25 -0300

    Projeto finalizado

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,34 @@ router.get('/newuser', function(req, res) {
   res.render('newuser', {title: 'Add new user'});
 })
 
+/*POST so that adds user Service*/
+router.post('/adduser', function(req,res){
+  //Setting our internal DB variable
+  var db = req.db;
+
+  //Get form values, especifically using "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  //Setting our collection
+  var collection = db.get('usercollection');
+
+  //Submit to the database
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  },
+  function (err, doc){
+    if(err){
+      //If fail, return error
+      res.send("We have run into a problem while sending information to our database. Please try again.")
+    }
+    else{
+      //Sending user to the success page
+      res.redirect('userlist');
+    }
+  }
+  );
+});
+
 module.exports = router;
